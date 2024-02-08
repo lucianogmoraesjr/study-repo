bad5639b1cbd3743551c56764512883222010a66
Luciano Moraes Jr. <lucianogmoraesjr@gmail.com>
2024-02-07 22:25:19 -0300

    :rocket: project finished

--- rocketseat/nlw/14-polls/src/http/server.ts
+++ rocketseat/nlw/14-polls/src/http/server.ts
@@ -1,9 +1,11 @@
 import fastify from 'fastify'
 import cookie from '@fastify/cookie'
+import { fastifyWebsocket } from '@fastify/websocket'
 
 import { createPoll } from './routes/create-poll'
 import { getPoll } from './routes/get-poll'
 import { voteOnPoll } from './routes/vote-on-poll'
+import { pollResults } from './ws/poll-results'
 
 const app = fastify()
 
@@ -12,9 +14,12 @@ app.register(cookie, {
   hook: 'onRequest',
 })
 
+app.register(fastifyWebsocket)
+
 app.register(createPoll)
 app.register(getPoll)
 app.register(voteOnPoll)
+app.register(pollResults)
 
 app.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
   console.log('🔥 server running on http://localhost:3333')
